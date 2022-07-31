8e97d802f9daace35d7084aa4372ec61d794120d
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-07-31 11:02:57 -0600

    Refactor codes

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,17 +1,86 @@
 package main;
 
-public class Game {
+import entities.Player;
+
+import java.awt.*;
+
+public class Game implements Runnable {
     private Window window;
     private Screen screen;
+    private Thread thread;
+    private Player player;
+    private final int FPS_SET = 120;
+    private final int UPS_SET = 200;
 
     public Game() {
         initClasses();
 
         screen = new Screen(this);
         window = new Window(screen);
+
+        start();
     }
 
     private void initClasses() {
+        player = new Player(100, 100, 4);
+    }
 
+    private void start() {
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public void update() {
+        player.update();
+    }
+
+    public void draw(Graphics2D graphics2D) {
+        player.draw(graphics2D);
+    }
+
+    @Override
+    public void run() {
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        double timePerUpdate = 1000000000.0 / UPS_SET;
+        long lastTime = System.nanoTime();
+
+        int frames = 0;
+        int updates = 0;
+        long lastCheck = System.currentTimeMillis();
+
+        double deltaUpdates = 0;
+        double deltaFrames = 0;
+
+        while(thread != null) {
+            long currentTime = System.nanoTime();
+            deltaUpdates += (currentTime - lastTime) / timePerUpdate;
+            deltaFrames += (currentTime - lastTime) / timePerFrame;
+
+            lastTime = currentTime;
+            // UPDATE: update information such as character positions
+            if(deltaUpdates >= 1) {
+                update();
+                updates++;
+                deltaUpdates--;
+            }
+
+            // DRAW: draw the screen with the updated information
+            if(deltaFrames >= 1) {
+                screen.repaint();
+                frames++;
+                deltaFrames--;
+            }
+
+            if(System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames + " | UPS: " + updates);
+                frames = 0;
+                updates = 0;
+            }
+        }
+    }
+
+    public Player getPlayer() {
+        return player;
     }
 }
--- src/main/Screen.java
+++ src/main/Screen.java
@@ -10,14 +10,10 @@ import java.awt.*;
 
 import static utilities.Constants.ScreenConstants.*;
 
-public class Screen extends JPanel implements Runnable {
+public class Screen extends JPanel  {
     private Game game;
-    private Thread thread;
     private MouseHandler mouseHandler;
-    private Player player;
     private TileManager tileManager;
-    private final int FPS_SET = 120;
-    private final int UPS_SET = 200;
 
     public Screen(Game game) {
         initClasses();
@@ -31,11 +27,9 @@ public class Screen extends JPanel implements Runnable {
         addMouseMotionListener(mouseHandler);
         setFocusable(true);
         requestFocus();
-        start();
     }
 
     private void initClasses() {
-        player = new Player(100, 100, 4);
         tileManager = new TileManager(this);
     }
 
@@ -45,66 +39,15 @@ public class Screen extends JPanel implements Runnable {
         System.out.println("Size: " + SCREEN_WIDTH + ", " + SCREEN_HEIGHT);
     }
 
-    private void start() {
-        thread = new Thread(this);
-        thread.start();
-    }
-
-    public void update() {
-        player.update();
-    }
-
     public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
         Graphics2D graphics2D = (Graphics2D) graphics;
         tileManager.draw(graphics2D);
-        player.draw(graphics2D);
+        game.draw(graphics2D);
         graphics2D.dispose();
     }
 
-    @Override
-    public void run() {
-        double timePerFrame = 1000000000.0 / FPS_SET;
-        double timePerUpdate = 1000000000.0 / UPS_SET;
-        long lastTime = System.nanoTime();
-
-        int frames = 0;
-        int updates = 0;
-        long lastCheck = System.currentTimeMillis();
-
-        double deltaUpdates = 0;
-        double deltaFrames = 0;
-
-        while(thread != null) {
-            long currentTime = System.nanoTime();
-            deltaUpdates += (currentTime - lastTime) / timePerUpdate;
-            deltaFrames += (currentTime - lastTime) / timePerFrame;
-
-            lastTime = currentTime;
-            // UPDATE: update information such as character positions
-            if(deltaUpdates >= 1) {
-                update();
-                updates++;
-                deltaUpdates--;
-            }
-
-            // DRAW: draw the screen with the updated information
-            if(deltaFrames >= 1) {
-                repaint();
-                frames++;
-                deltaFrames--;
-            }
-
-            if(System.currentTimeMillis() - lastCheck >= 1000) {
-                lastCheck = System.currentTimeMillis();
-                System.out.println("FPS: " + frames + " | UPS: " + updates);
-                frames = 0;
-                updates = 0;
-            }
-        }
-    }
-
-    public Player getPlayer() {
-        return player;
+    public Game getGame() {
+        return game;
     }
 }
