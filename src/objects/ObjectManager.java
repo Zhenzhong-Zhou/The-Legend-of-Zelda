97b5a39b281106d76eca8d823806d9db84cc1055
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-07-31 21:52:43 -0600

    Draw new objects

--- src/objects/ObjectManager.java
+++ src/objects/ObjectManager.java
@@ -14,6 +14,7 @@ public class ObjectManager {
     }
 
     public void setObjects() {
+        // Keys
         screen.objects[0] = new Object_Key();
         screen.objects[0].worldX = 27 * TILE_SIZE;
         screen.objects[0].worldY = 8 * TILE_SIZE;
@@ -21,6 +22,28 @@ public class ObjectManager {
         screen.objects[1] = new Object_Key();
         screen.objects[1].worldX = 27 * TILE_SIZE;
         screen.objects[1].worldY = 40 * TILE_SIZE;
+
+        screen.objects[2] = new Object_Key();
+        screen.objects[2].worldX = 42 * TILE_SIZE;
+        screen.objects[2].worldY = 11 * TILE_SIZE;
+
+        // Doors
+        screen.objects[3] = new Object_Door();
+        screen.objects[3].worldX = 14 * TILE_SIZE;
+        screen.objects[3].worldY = 12 * TILE_SIZE;
+
+        screen.objects[4] = new Object_Door();
+        screen.objects[4].worldX = 12 * TILE_SIZE;
+        screen.objects[4].worldY = 29 * TILE_SIZE;
+
+        screen.objects[5] = new Object_Door();
+        screen.objects[5].worldX = 16 * TILE_SIZE;
+        screen.objects[5].worldY = 22 * TILE_SIZE;
+
+        // Chest
+        screen.objects[6] = new Object_Chest();
+        screen.objects[6].worldX = 14 * TILE_SIZE;
+        screen.objects[6].worldY = 9 * TILE_SIZE;
     }
 
     public void draw(Graphics2D graphics2D) {
