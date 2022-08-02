d49217cf07f4b277f20a9f7d74834467f5104963
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-08-02 12:12:42 -0600

    Add game loop

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,12 +1,70 @@
 package main;
 
-public class Game {
+import static utility.Constant.GameConstant.FPS_SET;
+import static utility.Constant.GameConstant.UPS_SET;
+
+public class Game implements Runnable{
     private Window window;
     private Scene scene;
+    private Thread thread;
 
     public Game() {
         scene = new Scene();
         window = new Window(scene);
+        scene.setFocusable(true);
         scene.requestFocus();
+
+        start();
+    }
+
+    public void update() {
+
+    }
+
+    private void start() {
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        double timePerUpdate = 1000000000.0 / UPS_SET;
+        long lastTime = System.nanoTime();
+
+        int frames = 0;
+        int updates = 0;
+        long lastCheck = System.currentTimeMillis();
+
+        double deltaUpdates = 0;
+        double deltaFrames = 0;
+
+        while(thread != null) {
+            long currentTime = System.nanoTime();
+            deltaUpdates += (currentTime - lastTime) / timePerUpdate;
+            deltaFrames += (currentTime - lastTime) / timePerFrame;
+
+            lastTime = currentTime;
+            // UPDATE: update information such as character positions
+            if(deltaUpdates >= 1) {
+                update();
+                updates++;
+                deltaUpdates--;
+            }
+
+            // DRAW: draw the screen with the updated information
+            if(deltaFrames >= 1) {
+                scene.repaint();
+                frames++;
+                deltaFrames--;
+            }
+
+            if(System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames + " | UPS: " + updates);
+                frames = 0;
+                updates = 0;
+            }
+        }
     }
 }
