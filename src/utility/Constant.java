c3fa66fb1af929657f6ef090c2072738fa6c48ed
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-08-03 18:45:52 -0600

    Fix errors

--- src/utility/Constant.java
+++ src/utility/Constant.java
@@ -20,12 +20,12 @@ public class Constant {
             Actual Size
          */
         public static final int TILE_SIZE = (int) (ORIGINAL_TILES_SIZE * SCALE);    // 48*48 tile
-        // @formatter:on
-        public static final int SCENE_WIDTH = TILE_SIZE * MAX_SCREEN_COL;       // 768 pixels
-        public static final int SCENE_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW;      // 576 pixels
         // @formatter:off
         public static final int MAX_SCREEN_COL = 20;        // ratio 4:3
         public static final int MAX_SCREEN_ROW = 15;        // ratio 4:3
+        // @formatter:on
+        public static final int SCENE_WIDTH = TILE_SIZE * MAX_SCREEN_COL;       // 768 pixels
+        public static final int SCENE_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW;      // 576 pixels
     }
 
     /**
